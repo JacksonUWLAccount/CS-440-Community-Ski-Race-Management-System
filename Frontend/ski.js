1d75bd15f54a3ac16eb9d8e415950a577c122002
Matt Dvoracek <23mdvoracek@gmail.com>
2026-03-02 12:42:00 -0600

    testing git ignore file again

---
+++ Frontend/ski.js
@@ -0,0 +1,61 @@
+function signup(username,email,password,firstName,lastName){
+
+    // let username = "mattdvora";
+    // let email = "mdvor4@gmail.com";
+    // let password = "Password";
+    // let firstName = "Ean";
+    // let lastName = "Mares";
+
+    fetch("http://localhost:8080/api/signup", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
+    })
+    .then(res => res.json())
+    .then(success => { console.log(success);});
+}
+
+function login(username, password){
+
+    username = "mattdvora";
+    password = "Password";
+
+    fetch("http://localhost:8080/api/login", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        username: username,
+        password: password,
+    })
+    })
+    .then(res => res.json())
+    .then(success => { console.log(success);});
+}
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("signup").addEventListener("click", function(){
+        let username = document.getElementById("usernameIn").value;
+        let email = document.getElementById("emailIn").value;
+        let password = document.getElementById("passwordIn").value;
+        let firstName = document.getElementById("firstNameIn").value;
+        let lastName = document.getElementById("lastNameIn").value;
+        console.log(`${username}, ${email}, ${password}, ${firstName},${lastName}`);
+        signup(username,email,password,firstName,lastName);
+    });
+
+    document.getElementById("login").addEventListener("click", function(){
+
+        login();
+    });
+});
